10c771dd87cbc763f7ba44ac5dd3ec2e96a43917
Michael-Xie <xiemicha@gmail.com>
2020-11-30 09:43:43 -0500

    Start on MobileApp page

---
+++ src/components/MobileApps.js
@@ -0,0 +1,114 @@
+import React from "react";
+import Lottie from "react-lottie";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Hidden from "@material-ui/core/Hidden";
+
+import backArrow from "../assets/backArrow.svg";
+import forwardArrow from "../assets/forwardArrow.svg";
+
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    maxWidth: "40em",
+  },
+  arrowContainer: {
+    marginTop: "0.5em",
+  },
+  rowContainer: {
+    paddingLeft: "5em",
+    paddingRight: "5em",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "1.5em",
+      paddingRight: "1.5em",
+    },
+  },
+}));
+
+export default function MobileApps(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+  const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
+
+  return (
+    <Grid container direction="column">
+      <Grid
+        item
+        container
+        direction="row"
+        justify={matchesMD ? "center" : undefined}
+        className={classes.rowContainer}
+        style={{ marginTop: matchesXS ? "1em" : "2em" }}
+      >
+        <Hidden mdDown>
+          <Grid
+            item
+            className={classes.arrowContainer}
+            style={{ marginRight: "1em", marginLeft: "-3.5em" }}
+          >
+            <IconButton
+              style={{ backgroundColor: "transparent" }}
+              component={Link}
+              to="/services"
+              onClick={() => props.setSelectedIndex(0)}
+            >
+              <img src={backArrow} alt="Back to Services Page" />
+            </IconButton>
+          </Grid>
+        </Hidden>
+
+        <Grid item container direction="column" className={classes.heading}>
+          <Grid item>
+            <Typography align={matchesMD ? "center" : undefined} variant="h2">
+              iOS/Android App Development
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              align={matchesMD ? "center" : undefined}
+              variant="body1"
+              paragraph
+            >
+              Text 1 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+              aaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            </Typography>
+            <Typography
+              align={matchesMD ? "center" : undefined}
+              variant="body1"
+              paragraph
+            >
+              Text 2
+            </Typography>
+            <Typography
+              align={matchesMD ? "center" : undefined}
+              variant="body1"
+              paragraph
+            >
+              Text 3
+            </Typography>
+          </Grid>
+        </Grid>
+        <Hidden mdDown>
+          <Grid item className={classes.arrowContainer}>
+            <IconButton
+              style={{ backgroundColor: "transparent" }}
+              component={Link}
+              to="/mobileapps"
+              onClick={() => props.setSelectedIndex(2)}
+            >
+              <img
+                src={forwardArrow}
+                alt="Forward to iOS/Android App Development"
+              />
+            </IconButton>
+          </Grid>
+        </Hidden>
+      </Grid>
+    </Grid>
+  );
+}
