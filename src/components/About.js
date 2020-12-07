eb0cbcb54119e3e8cca72354bf57cce4dac321d4
Michael-Xie <xiemicha@gmail.com>
2020-12-07 09:19:56 -0500

    Add more components to About page

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,13 @@ import { makeStyles, useTheme } from "@material-ui/core/styles";
 
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 import history from "../assets/history.svg";
+import profile from "../assets/founder.jpg";
+import yearbook from "../assets/yearbook.svg";
+import puppy from "../assets/puppy.svg";
 
 import CallToAction from "./ui/CallToAction";
 
@@ -34,6 +38,10 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "1.5rem",
     maxWidth: "50em",
     lineHeight: 1.4,
+  },
+  avatar: {
+    height: "25em",
+    width: "25em",
   },
 }));
 
@@ -114,6 +122,64 @@ export default function Websites(props) {
               alt="quill pen sitting on top of book"
               style={{ maxHeight: "22em" }}
             />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction="column"
+        alignItems="center"
+        className={classes.rowContainer}
+      >
+        <Grid item>
+          <Typography variant="h4" align="center" gutterBottom>
+            Team
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1" paragraph align="center">
+            Zachary Reece, Founder
+          </Typography>
+          <Typography variant="body1" paragraph align="center">
+            I started coding when I was 9 years old.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Avatar alt="founder" src={profile} className={classes.avatar} />
+        </Grid>
+        <Grid item container>
+          <Grid item container direction="column" lg>
+            <Grid item>
+              <img src={yearbook} alt="yearbook page about founder" />
+            </Grid>
+            <Grid item>
+              <Typography variant="caption">
+                a page from my Sophomore yearbook
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item lg style={{ maxWidth: "45em", padding: "1.25em" }}>
+            <Typography variant="body1" align="center" paragraph>
+              a page from my Sophomore yearbook a page from my Sophomore
+              yearbook a page from my Sophomore yearbook a page from my
+              Sophomore yearbook
+            </Typography>
+            <Typography variant="body1" align="center" paragraph>
+              a page from my Sophomore yearbook a page from my Sophomore
+              yearbook a page from my Sophomore yearbook a page from my
+              Sophomore yearbook
+            </Typography>
+          </Grid>
+          <Grid item container direction="column" alignItems="flex-end" lg>
+            <Grid item>
+              <img src={puppy} alt="grey spotted puppy" />
+            </Grid>
+            <Grid item>
+              <Typography variant="caption">
+                my miniature dapple dachshund Sterling
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
